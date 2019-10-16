c192d3212253a71f64818cbf39c239d2cd09c59f
Minh Vu <vutranminh@gmail.com>
2019-10-16 17:08:37 +1300

    create data access library

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-
+const {accounts, users, writeJSON} = require('./data');
 const express = require('express');
+
 
 const app = express();
 
@@ -11,13 +12,6 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 
-const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
-
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
-
-const users = JSON.parse(userData);
 
 app.get('/', (req, res) => {
     res.render('index', {'title': 'Account Summary', accounts});
@@ -40,8 +34,7 @@ app.get('/transfer', (req, res) => {
 app.post('/transfer', (req, res) => {
     accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-    const accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('transfer', {message: 'Transfer Completed'});
 });
 
@@ -49,9 +42,7 @@ app.get('/payment', (req, res) => res.render('payment', {account: accounts.credi
 app.post('/payment', (req, res) => {
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount, 10);
-    const accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
-
+    writeJSON();
     res.render('payment', {message: 'Payment Successful', account: accounts.credit});
 })
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
